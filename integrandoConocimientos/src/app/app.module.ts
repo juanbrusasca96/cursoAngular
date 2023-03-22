7c27d9bac24a1aad3af46d4c4e7844e175957d1c
Juan Brusasca <juanbrusasca96@gmail.com>
2023-03-22 13:33:57 -0300

    styles

--- integrandoConocimientos/src/app/app.module.ts
+++ integrandoConocimientos/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    HomeComponent,
+    PageWrapperComponent
   ],
   imports: [
     BrowserModule
