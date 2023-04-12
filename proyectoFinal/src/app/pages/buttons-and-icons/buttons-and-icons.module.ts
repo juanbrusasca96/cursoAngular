496af5eaa7e75ffd9f2d1fd87bbc59f23f6032fc
Juan Brusasca <juanbrusasca96@gmail.com>
2023-04-12 11:20:44 -0300

    buttons and icons component

---
+++ proyectoFinal/src/app/pages/buttons-and-icons/buttons-and-icons.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonsAndIconsComponent } from './buttons-and-icons.component';
+
+
+
+@NgModule({
+  declarations: [
+    ButtonsAndIconsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ButtonsAndIconsComponent
+  ]
+})
+export class ButtonsAndIconsModule { }
