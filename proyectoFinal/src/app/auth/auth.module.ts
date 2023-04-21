85a2b7a5818395a290100e906234703ba9b546e9
Juan Brusasca <juanbrusasca96@gmail.com>
2023-04-21 17:10:25 -0300

    auth component

---
+++ proyectoFinal/src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { AuthComponent } from './auth.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    AuthComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    AuthComponent
+  ]
+})
+export class AuthModule { }
