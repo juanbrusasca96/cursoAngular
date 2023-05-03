84a8f573462beb8c3ecbae91f6bd2d048bb17d9c
Juan Brusasca <juanbrusasca96@gmail.com>
2023-05-03 14:22:07 -0300

    login component

---
+++ proyectoFinal/src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService, LoginFormValue } from 'src/app/core/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  emailControl = new FormControl('', [Validators.required]);
+  passwordControl = new FormControl('', [Validators.required]);
+  loginForm = new FormGroup({
+    email: this.emailControl,
+    password: this.passwordControl
+  })
+
+  constructor(private authService: AuthService, private activatedRoute: ActivatedRoute) {
+    console.log(activatedRoute.snapshot);
+
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+    }
+    else {
+      this.authService.login(this.loginForm.value as LoginFormValue)
+    }
+  }
+}
