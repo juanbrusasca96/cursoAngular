9a2c92005cf3482e2e33915947847af790be273f
Juan Brusasca <juanbrusasca96@gmail.com>
2023-05-03 14:21:54 -0300

    auth guards

---
+++ proyectoFinal/src/app/auth/guards/login.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { AuthService } from 'src/app/core/services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return this.authService.verificarToken()
+      .pipe(
+        map((usuarioAutenticado) => {
+          if (usuarioAutenticado) {
+            return this.router.createUrlTree(['dashboard']);
+          } else {
+            return true;
+          }
+        })
+      )
+  }
+
+}
