93e3506ff0d11abb4b0e1d95e9e8140383380ba6
Juan Brusasca <juanbrusasca96@gmail.com>
2023-05-31 22:31:16 -0300

    second base url

--- proyectoFinal/src/enviroments/enviroments.ts
+++ proyectoFinal/src/enviroments/enviroments.ts
@@ -1,4 +1,5 @@
 export const enviroment = {
     isProduction: false,
     apiBaseUrl: 'https://64529122a2860c9ed40fd60e.mockapi.io',
+    apiSecondBaseUrl: 'https://647798339233e82dd53be6c9.mockapi.io'
   }
